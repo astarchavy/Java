48abdeb0348a28da67c28480f62fba4a261408c1
astarchavy <astarchavy@gmail.com>
2021-05-02 20:28:35 +0300

    first version

---
+++ inheritance2_2/src/by/gsu/epamlab/Byn.java
@@ -0,0 +1,60 @@
+package by.gsu.epamlab;
+
+public final class Byn implements Comparable<Byn> {
+    private final int kopecks;
+
+    public Byn() {
+        this(0);
+    }
+
+    public Byn(int kopecks) {
+        this.kopecks = kopecks;
+    }
+
+    public Byn(int rubs, int kopecks) {
+        this.kopecks = rubs * 100 + kopecks;
+    }
+
+    public Byn(Byn byn) {
+        this.kopecks = byn.kopecks;
+    }
+
+    public Byn add(Byn byn) {
+        return new Byn(kopecks + byn.kopecks);
+    }
+
+    public Byn multiply(int secondFactor) {
+        return new Byn(kopecks * secondFactor);
+    }
+
+    public Byn multiply(double k, int d) {
+        return new Byn((int) (kopecks * k), d);
+    }
+
+    public Byn subtraction(Byn byn) {
+        return new Byn(kopecks - byn.kopecks);
+    }
+
+    public Byn round(RoundMethod roundMethod, int d) {
+        return new Byn(roundMethod.round(kopecks, d));
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%d.%02d", kopecks / 100, kopecks % 100);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Byn)) return false;
+        Byn byn = (Byn) o;
+        return kopecks == byn.kopecks;
+    }
+
+    @Override
+    public int compareTo(Byn o) {
+        return kopecks - o.kopecks;
+    }
+}
---
+++ inheritance2_2/src/by/gsu/epamlab/Product.java
@@ -0,0 +1,25 @@
+package by.gsu.epamlab;
+
+public class Product {
+
+    private final String name;
+    private final Byn price;
+
+    public Product(String name, Byn price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Byn getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + price;
+    }
+}
